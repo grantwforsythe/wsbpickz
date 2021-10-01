bbdf396d8982a39aa3f56cc4e6193294bbdf5ba9
Grant Forsythe <grantwforsythe@gmail.com>
2021-09-30 23:54:38 -0400

    Update app

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,4 @@
-import os
-import praw
-from dotenv import load_dotenv
 from flask import Flask
-
-load_dotenv()
-
-reddit = praw.Reddit(
-    client_id=os.getenv('CLIENT_ID'),
-    client_secret=os.getenv('CLIENT_SECRET'),
-    user_agent=os.getenv('USER_AGENT'),
-)
 
 app = Flask(__name__)
 
